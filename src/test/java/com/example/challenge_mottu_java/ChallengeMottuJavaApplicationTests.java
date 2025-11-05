eeb97fc279c9991cf7beeb8b2de5d1d4e4ded473
Rafael Souza <rafaelsouza2017a@gmail.com>
2025-11-05 16:21:55 -0300

    Adding test

--- src/test/java/com/example/challenge_mottu_java/ChallengeMottuJavaApplicationTests.java
+++ src/test/java/com/example/challenge_mottu_java/ChallengeMottuJavaApplicationTests.java
@@ -2,10 +2,12 @@ package com.example.challenge_mottu_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChallengeMottuJavaApplicationTests {
 
     @Test
